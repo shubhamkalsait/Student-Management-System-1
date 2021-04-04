cd3fab86edbdcc3f051f8bb42a55c862b57418bd
shubham.kalsait <ash.shubham.kalsait@gmail.com>
2021-04-04 12:19:28 +0530

    db.php updated

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+                define('db_host', 'ec2-65-1-131-138.ap-south-1.compute.amazonaws.com');
+                define('db_user', 'admin');
+                define('db_pass', 'admin');
+                define('db_name', 'student_info');
+        ?>
