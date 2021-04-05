0d36c5f8c665a50e5cc6be764838861404ce87f6
shubham.kalsait <ash.shubham.kalsait@gmail.com>
2021-04-05 22:21:26 +0530

    app updated

--- page/user/user_list/side_bar.php
+++ page/user/user_list/side_bar.php
@@ -8,7 +8,7 @@
         	<li onclick="get_user_list('all')" class="nav_button"><span class="glyphicon glyphicon-user"></span> All User List</li>
         	<li onclick="get_user_list('active')"class="nav_button"><span class="glyphicon glyphicon-user"></span> Active User List</li>
         	<li onclick="get_user_list('deactive')" class="nav_button"><span class="glyphicon glyphicon-user"></span> Deactive User List</li>
-        	<li onclick="get_user_list('techserm')" class="nav_button"><span class="glyphicon glyphicon-user"></span> TechSerm User List</li>
+        	<li onclick="get_user_list('techserm')" class="nav_button"><span class="glyphicon glyphicon-user"></span> CloudBlitz User List</li>
         	<li onclick="get_user_list('institute')" class="nav_button"><span class="glyphicon glyphicon-user"></span> Institute User List</li>
          </div>           
         </ul> 
