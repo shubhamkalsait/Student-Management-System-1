3de49e6b2d5efe5ac005faebde25aa632fa4decf
Shubham Kalsait <34379398+shubhamkalsait@users.noreply.github.com>
2021-04-29 08:05:33 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-                define('db_host', 'ec2-65-1-131-138.ap-south-1.compute.amazonaws.com');
+                define('db_host', 'ec2-18-188-244-112.us-east-2.compute.amazonaws.com');
                 define('db_user', 'admin');
                 define('db_pass', 'admin');
                 define('db_name', 'student_info');
